e1d5c01a5a4684d15c2fe48a5db905d5aedda2b9
Anton <anton@adc.sh>
2018-12-04 05:44:16 +0300

    files

--- example/index.js
+++ example/index.js
@@ -1,5 +1,2 @@
-require('@babel/register')
-const { resolve } = require('path')
-
-const p = resolve(__dirname, '..', process.argv[2])
-require(p)
+require('alamode')()
+require(`../${process.argv[2]}`)
